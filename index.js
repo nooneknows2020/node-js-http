64e2a637624374a787896fb989d035cf517a6306
nooneknows2020 <e_dends@yahoo.co.jp>
2020-07-08 17:26:07 +0900

    HTMLフォームとHTMLメソッドの振り分けの練習

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info(`[${now}] Requested by ${req.connection.remoteAddress}]`);
   res.writeHead(200, {
-    'Content-type': 'text/plain; charset=utf-8'
+    'Content-type': 'text/html; charset=utf-8'
   });
   switch(req.method){
     case 'GET':
-      res.write(`GET ${req.url}`);
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res); //データを受け渡した後、通信が終了する
       break;
     case 'POST':
-      res.write(`POST ${req.url}`);
       let body = '';
       req.on('data', (chunk) => {
         body += chunk;
       }).on('end', () => {
-        console.info(`[${now}] Data posted: ${body}`);
+        const decoded = decodeURIComponent(body);
+        console.info(`[${now}] 投稿: ${decoded}`);
+        res.write(`<h1>${decoded}が投稿されました</h1>`);
+        res.end();  //POSTメソッドを使った場合のみ、res.end()を行う。
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) =>{
   console.error(`[${new Date()}] Server Error`, e);
 }).on('clientError', (e) =>{
